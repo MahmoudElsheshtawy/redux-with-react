b5d8c19e7d9240a45cf5f673bc3e83ae762155ef
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-02-03 19:19:16 -1200

    r

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { addproduct } from '../store/ACTION/action'
 
 const Products = () => {
-
+    const dispach =useDispatch()
     const products= useSelector((state)=>state.products)
     console.log(products);
   return (
     <>
     <h1>my products</h1>
-        
+        <button onClick={()=>dispach(addproduct({id:"2",title:"im nobody"}))}>addproduct</button>
 {
     products.map((product)=>(
         <div key={product.id}>
